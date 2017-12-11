767cbb91ea76acdc25015414ea1629390d5f2a00
Mark Gritter <mgritter@gmail.com>
2017-12-10 21:10:50 -0800

    Day 10 second half.

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -12,21 +12,35 @@ fn twist( x : & mut Vec<u32>, pos : usize, length : usize ) {
 }
 
 fn main() {
-    //let input = vec!( 3, 4, 1, 5 );
-    //let mut list : Vec<u32> = (0..5).collect();
-    let input = vec!(230,1,2,221,97,252,168,169,57,99,0,254,181,255,235,167);
+    let mut input : Vec<usize> =
+        "230,1,2,221,97,252,168,169,57,99,0,254,181,255,235,167"
+        .as_bytes().iter().map( |c| *c as usize ).collect();
+    let extra : Vec<usize> = vec!( 17, 31, 73, 47, 23 );
+    input.extend( extra );
+        
     let mut list : Vec<u32> = (0..256).collect();
+
     let mut pos : usize = 0;
     let mut skip_size : usize = 0;
-    
-    for length in input {
-        twist( &mut list, pos, length );
-        println!( "Twist: {} {:?}", length, list );
-        pos = (pos + length + skip_size) % list.len();
-        skip_size += 1;
-        println!( "Pos: {}, skip_size: {}", pos, skip_size );
+
+    for _round in 0..64 {
+        for length in input.iter() {
+            twist( &mut list, pos, *length );
+            // println!( "Twist: {} {:?}", length, list );
+            pos = (pos + length + skip_size) % list.len();
+            skip_size += 1;
+            // println!( "Pos: {}, skip_size: {}", pos, skip_size );
+        }
     }
     
     println!( "Final list: {:?}", list );
-    println!( "Product: {}", list[0] * list[1] );
+
+    let mut hash : String = String::from( "" );
+    for i in 0..16 {
+        let hv = list[i*16..(i+1)*16].iter().fold( 0, |acc, &x| acc ^ x );
+        hash.push_str( format!( "{:02x}", hv ).as_str() );
+    }
+
+    println!( "Hash value: {}", hash );
+       
 }
